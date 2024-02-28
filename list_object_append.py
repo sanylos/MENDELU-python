12ec4603ab8067b4aee17ea338e1602efe988b6a
Sanyl <129376896+sanylos@users.noreply.github.com>
2024-02-28 08:38:11 +0100

    Added list object append example

---
+++ list_object_append.py
@@ -0,0 +1,18 @@
+osoba1 = {
+    "jmeno":"Ferda",
+    "prijmeni":"Mravenec",
+    "datum_narozeni":"1978-08-08",
+    "pohlavi":"dělník"
+}
+
+osoba2 = {
+    "jmeno":"Adam",
+    "prijmeni":"Sanyl",
+    "datum_narozeni":"2024-01-02",
+    "pohlavi":"muž"
+}
+
+seznam = list()
+seznam.append(osoba1)
+seznam.append(osoba2)
+print(seznam)
